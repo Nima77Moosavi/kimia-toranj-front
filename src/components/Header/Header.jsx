3649ae821ceb61402e7df6e34b6e0c413803822a
Nima77Moosavi <nima77moosavi@gmail.com>
2025-03-06 11:47:08 +0330

    changed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Header.module.css";
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.png";
 import { IoMenu } from "react-icons/io5";
 import { TiHeart } from "react-icons/ti";
 import { FaCartShopping } from "react-icons/fa6";
@@ -8,27 +8,30 @@ import { FaCartShopping } from "react-icons/fa6";
 const Header = () => {
   return (
     <header className={styles.header}>
-      {/* Top Section */}
-      <div className={styles.topSection}>
-        <div className={styles.brand}>
-          <h1>کیمیا ترنج<img src={logo} alt="" className={styles.logo}/></h1>
+      {/* Left Section */}
+      <div className={styles.leftSection}>
+        <div className={styles.icons}>
+          <span>
+            <FaCartShopping />
+          </span>
+          <span>
+            <TiHeart />
+          </span>
         </div>
         <div className={styles.authButtons}>
           <button className={styles.button}>ورود | ثبت نام</button>
         </div>
       </div>
 
-      {/* Bottom Section */}
-      <div className={styles.bottomSection}>
+      {/* Right Section */}
+      <div className={styles.rightSection}>
         <div className={styles.hamburgerMenu}>
-          <span><IoMenu /></span>
+          <span>
+            <IoMenu />
+          </span>
         </div>
         <div className={styles.searchBox}>
           <input type="text" placeholder="جستجو کنید..." />
-        </div>
-        <div className={styles.icons}>
-          <span><FaCartShopping /></span>
-          <span><TiHeart /></span>
         </div>
       </div>
     </header>
