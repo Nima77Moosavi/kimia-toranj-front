854f8cc9f2471787beffb0f8334c233081450225
Nima77Moosavi <nima77moosavi@gmail.com>
2025-03-09 10:27:32 +0330

    admin panel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/logo.png";
 import { IoMenu } from "react-icons/io5";
 import { TiHeart } from "react-icons/ti";
 import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -11,27 +12,31 @@ const Header = () => {
       {/* Left Section */}
       <div className={styles.leftSection}>
         <div className={styles.icons}>
-          <span>
+          <span className={styles.icon}>
             <FaCartShopping />
           </span>
-          <span>
+          <span className={styles.icon}>
             <TiHeart />
           </span>
         </div>
-        <div className={styles.authButtons}>
+        <Link to="/login">
           <button className={styles.button}>ورود | ثبت نام</button>
-        </div>
+        </Link>
+      </div>
+
+      {/* Middle Logo */}
+      <div className={styles.brand}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <h1>برند شما</h1>
       </div>
 
       {/* Right Section */}
       <div className={styles.rightSection}>
-        <div className={styles.hamburgerMenu}>
-          <span>
-            <IoMenu />
-          </span>
-        </div>
         <div className={styles.searchBox}>
           <input type="text" placeholder="جستجو کنید..." />
+        </div>
+        <div className={styles.hamburgerMenu}>
+          <IoMenu />
         </div>
       </div>
     </header>
