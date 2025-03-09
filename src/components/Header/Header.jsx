575b3b87bf7a629c3d02b969fab3b3dd79a7ca4b
fatemehghsmi <ghasemi12794@gmail.com>
2025-03-09 15:10:03 +0330

    header changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,61 @@
 import React from "react";
 import styles from "./Header.module.css";
-import logo from "../../assets/logo.png";
 import { IoMenu } from "react-icons/io5";
-import { TiHeart } from "react-icons/ti";
+import { GoHeartFill } from "react-icons/go";
 import { FaCartShopping } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5"; // آیکن جستجو
 import { Link } from "react-router-dom";
+
+// مسیر عکس‌ها (فرضی)
+import image1 from "../../assets/banner1.png";
+import image3 from "../../assets/img.png";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      {/* Left Section */}
-      <div className={styles.leftSection}>
+      {/* دایره طوسی */}
+      <div className={styles.circle}></div>
+
+      <div className={styles.container}>
+        {/* آیکن‌ها */}
         <div className={styles.icons}>
-          <span className={styles.icon}>
+          <span>
             <FaCartShopping />
           </span>
-          <span className={styles.icon}>
-            <TiHeart />
+          <span>
+          <GoHeartFill />
           </span>
         </div>
-        <Link to="/login">
-          <button className={styles.button}>ورود | ثبت نام</button>
-        </Link>
-      </div>
 
-      {/* Middle Logo */}
-      <div className={styles.brand}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-        <h1>برند شما</h1>
-      </div>
-
-      {/* Right Section */}
-      <div className={styles.rightSection}>
-        <div className={styles.searchBox}>
-          <input type="text" placeholder="جستجو کنید..." />
+        {/* دکمه ورود/ثبت نام */}
+        <div className={styles.loginButton}>
+          <Link to="/login">
+            <button>ورود | ثبت نام</button>
+          </Link>
         </div>
+
+        {/* جعبه جستجو */}
+        <div className={styles.searchContainer}>
+          <div className={styles.searchBox}>
+            <input type="text" placeholder="جستجو کنید..." />
+            <span className={styles.searchIcon}>
+              <IoSearch />
+            </span>
+          </div>
+        </div>
+
+        {/* منوی همبرگر */}
         <div className={styles.hamburgerMenu}>
-          <IoMenu />
+          <span>
+            <IoMenu />
+          </span>
         </div>
+      </div>
+
+      {/* عکس‌ها */}
+      <div className={styles.images}>
+        <img src={image3} alt="Image 3" className={styles.dast} />
+        <img src={image1} alt="Image 1" className={styles.txt1} />
       </div>
     </header>
   );
